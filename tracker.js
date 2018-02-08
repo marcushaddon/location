2a68fd6f495a4874c36e14ccee7fb10e1acf9e17
marcushaddon <haddon.marcus@gmail.com>
2018-02-08 17:38:14 -0600

    Add drop pin feature.

--- tracker.js
+++ tracker.js
@@ -1,22 +1,37 @@
 "use strict";
+let currentLocation = {};
+let pin = null;
 
-// function locationLoop(model) {
-//     navigator.geolocation.getCurrentPosition(
-//         (pos) => {
-//             model.position = pos;
-//             console.log(model);
-//             setTimeout(() => { locationLoop(model); }, 1000);
-//         }
-//     );
-// }
+function main() {
+    navigator.geolocation.watchPosition((pos) => {
+        console.log(pos);
+        currentLocation = pos.coords;
+        document.getElementById('monitor').innerHTML = `<h3>lat: ${pos.coords.latitude}</h3>
+                                                        <h3>long: ${pos.coords.longitude}</h3>
+                                                        <h3>speed: ${pos.coords.speed}</h3>`;
+        document.getElementById('distancetopin').innerHTML = distanceToPin();
+    });
+}
 
-// var model = {};
+function distanceToPin() {
+    if (pin === null) {
+        return Infinity;
+    }
+    let x1 = pin.latitude;
+    let x2 = currentLocation.latitude;
+    let y1 = pin.longitude;
+    let y2 = currentLocation.longitude;
+    let distance = Math.sqrt(
+        Math.pow((x2 - x1), 2) - Math.pow((y2 - y1), 2)
+    );
+    
+    return distance;
 
-// locationLoop(model);
+}
 
-navigator.geolocation.watchPosition((pos) => {
-    console.log(pos);
-    document.getElementById('monitor').innerHTML = `<h3>lat: ${pos.coords.latitude}</h3>
-                                                    <h3>long: ${pos.coords.longitude}</h3>
-                                                    <h3>speed: ${pos.coords.speed}</h3>`;
+document.getElementById('drop-pin').addEventListener('click', () => {
+    pin = currentLocation;
+    
 });
+
+main();
