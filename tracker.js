221b9c2e629094d29cadef4423fe3dc5c044871c
marcushaddon <haddon.marcus@gmail.com>
2018-02-08 18:51:38 -0600

    Display stuff.

--- tracker.js
+++ tracker.js
@@ -9,7 +9,7 @@ function main() {
         document.getElementById('monitor').innerHTML = `<h3>lat: ${pos.coords.latitude}</h3>
                                                         <h3>long: ${pos.coords.longitude}</h3>
                                                         <h3>speed: ${pos.coords.speed}</h3>`;
-        document.getElementById('distancetopin').innerHTML = distanceToPin();
+        udpateDistanceToPin();
     });
 }
 
@@ -21,12 +21,29 @@ function distanceToPin() {
     let x2 = currentLocation.latitude;
     let y1 = pin.longitude;
     let y2 = currentLocation.longitude;
+    
     let distance = Math.sqrt(
         Math.pow((x2 - x1), 2) - Math.pow((y2 - y1), 2)
     );
+
+    document.getElementById('console').innerHTML = `
+        <ul>   
+            <li>x1: ${x1}</li>
+            <li>x2: ${x2}</li>
+            <li>y1: ${y1}</li>
+            <li>y2: ${y2}</li>
+            <li>partA: ${Math.pow((x2 - x1), 2)}</li>
+            <li>partA: ${Math.pow((y2 - y1), 2)}</li>
+        </ul>
+    `;
+
     
     return distance;
 
+}
+
+function udpateDistanceToPin() {
+    document.getElementById('distancetopin').innerHTML = distanceToPin();
 }
 
 document.getElementById('drop-pin').addEventListener('click', () => {
